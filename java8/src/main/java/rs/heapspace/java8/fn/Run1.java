2d98e237964faad28afc34ea5519a981c3edf113
Igor Spasić <igor@heapspace.rs>
2015-02-16 23:59:16 +0100

    Added examples

--- java8/src/main/java/rs/heapspace/java8/fn/Run1.java
+++ java8/src/main/java/rs/heapspace/java8/fn/Run1.java
@@ -3,6 +3,23 @@ package rs.heapspace.java8.fn;
 public class Run1 {
 
 	public static void main(String[] args) {
+//		anonymous class
+//		BasicFunction fn = new BasicFunction() {
+//			@Override
+//			public void doSomething() {
+//				StaticFunction.pureFun();
+//			}
+//		};
+
+		// lambda 1
+//		BasicFunction fn = () -> {
+//			StaticFunction.pureFun();
+//		};
+
+		// lambda 2
+//		BasicFunction fn = () -> StaticFunction.pureFun();
+
+		// method reference
 		BasicFunction fn = StaticFunction::pureFun;
 		fn.doSomething();
 		doItTwice(fn);
